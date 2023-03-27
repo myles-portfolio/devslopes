a6eef823158fa50c7b8280eff7434b6fa11ca002
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-03-27 18:42:44 -0500

    learned js variable types

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,4 +1,64 @@
-    var name = 'Tony';
-    var lastName = 'Hawk';
-    var fullName = name + ' ' + lastName;
-    console.log(fullName);
+    // Variables in JavaScript
+
+    // (var) - Prior ES2015 / ES6
+    var userName = 'Mylesh';
+    var totalWins = 30;
+    var totalLosses = 10;
+
+    // (let, const) - ES2015 / ES6
+    let name = 'Myles'
+    const userAge = 33;
+
+    /* Variable Rules */
+
+    // Should have a meaningful name
+    // Cannot contain a hyphen (-) or space
+    // Cannot start with a number
+    // Cannot use a reserved word
+    // Variables are case sensitive
+
+    // JavaScript Data Types
+
+    /* Primitive / Value Types */
+
+    var firstName = 'Mary'; // string
+    var age = 33; // number
+    var recedingHairline = false; // boolean
+    var favFood;// undefined
+    var toiletPaper = null; // null
+
+    toiletPaper = 24;
+    toiletPaper = null;
+    toiletPaper = 'Triple Ply';
+
+    /* Reference Types / Structural Types */
+
+    // Objects
+    var person = {
+        firstName: 'Mary',
+        userAge: 33,
+        recedingHairline: false,
+        favFood,
+        toiletPaper: 206,
+    }
+    // Arrays
+    var students = ['Orion', 'Kingston', 'Shayla', 'Chloe'];
+
+    // Functions
+    var myFunc = function() {
+        // rune some code
+    }
+    
+    console.log('Value: ', typeof myFunc);
+    console.log('Type: ', typeof myFunc);
+
+    /* Exercise: Variables */
+
+    var vehicleMake = 'Toyota';
+    var vehicleModel = 'Tacoma';
+    var vehicleYear = '2019';
+    var vehicleColor = 'White';
+    var isManualTransmission = false;
+    var numSeats = 4;
+    var mileage = 40345;
+    
