fb3d8c49798ed5173485de2e8389ea224073fe2e
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-04-17 12:10:50 -0500

    finished node calculator project

--- node-calculator/index.js
+++ node-calculator/index.js
@@ -1,35 +1,82 @@
 var rs = require('readline-sync');
 
-//TODO After a valid number is entered, ask the user, "Please enter the second number". Perform the same error handling as before
-//TODO Then create a function to perform the proper math operation and print the result as: "The result is: X" where "X" is the actual result
-
 // *** BONUS ***
 
-//TODO Handle remainders properly (modulo). Example : 9 divided by 4 would print 2.25
 //TODO Make the program more elegant by allowing the user to enter an entire operation on one line such as: "6 / 6" or "5 * 3" (Hint use arguments to solve this)
 
-const options = ['/','*','+','-']
+const options = ['/','*','+','-'];
+//var anotherOperation = false;
 
 var askForOperation = rs.question('What operation would you like to perform? ', {
   limit: options,
   limitMessage: 'That is not a valid operation.'
 });
 
-function getFirstNum() {
-  firstInput = rs.questionInt('Please enter the first number: ',
+var operator = askForOperation;
+
+var firstNumber = rs.questionInt('Please enter the first number: ',
+{
+limit: Number,
+limitMessage: 'This is not a number.'
+});
+
+var secondNumber = rs.questionInt('Please enter the second number: ',
+{
+limit: Number,
+limitMessage: 'This is not a number.'
+});
+
+function repeat() {
+  var askForOperation = rs.question('What operation would you like to perform? ', {
+    limit: options,
+    limitMessage: 'That is not a valid operation.'
+  });
+
+  operator = askForOperation;
+
+  firstNumber = rs.questionInt('Please enter the first number: ',
   {
   limit: Number,
   limitMessage: 'This is not a number.'
   });
-}
 
-function getSecondNum() {
-  secondInput = rs.questionInt('Please enter the second number: ',
+  secondNumber = rs.questionInt('Please enter the second number: ',
   {
   limit: Number,
   limitMessage: 'This is not a number.'
   });
+
+  calcResult();
+  askAgain();
 }
 
-var firstNumber = getFirstNum();
-var secondNumber = getSecondNum();
+function calcResult() {
+  if (operator === '/') {
+    let result = firstNumber / secondNumber;
+    console.log('The result is: ' + result);
+  } 
+  else if (operator === '*') {
+    let result = firstNumber * secondNumber;
+    console.log('The result is: ' + result);
+  }
+  else if (operator === '+') {
+    let result = firstNumber + secondNumber;
+    console.log('The result is: ' + result);
+  }
+  else if (operator === '-') {
+    let result = firstNumber - secondNumber;
+    console.log('The result is: ' + result);
+  }
+}
+
+function askAgain() {
+  let anotherOperation = rs.keyInYN('Would you like to perform another operation? ')
+  if (anotherOperation === true) {
+    repeat();
+  } else {
+    console.log('Thank you for trying my calculator!');
+  }
+}
+
+calcResult();
+askAgain();
