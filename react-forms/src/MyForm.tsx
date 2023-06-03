dc9f8da6ed61515ddbab62afac30c0db9c26338c
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-06-03 09:45:16 -0500

    more learning of react forms

--- react-forms/src/MyForm.tsx
+++ react-forms/src/MyForm.tsx
@@ -1,17 +1,57 @@
 import React from "react";
 
-class MyForm extends React.Component {
-	onSubmit = (event: { target: { [x: string]: { value: string } } }) => {
-		console.log(event.target["name"].value);
+interface MyFormState {
+	value: string;
+}
+
+class MyForm extends React.Component<object, MyFormState> {
+	constructor(props: object) {
+		super(props);
+		this.state = { value: "" };
+	}
+
+	onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+		event.preventDefault();
+		console.log(this.state.value);
+		this.setState({ value: "" });
+	};
+
+	onChange = (
+		event: React.ChangeEvent<
+			HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+		>
+	) => {
+		event.preventDefault();
+		const value = event.target.value;
+		this.setState({ value: value });
 	};
 
 	render() {
 		return (
-			<form>
+			<form className="myForm" onSubmit={this.onSubmit}>
 				<label>
 					Name:
-					<input type="text" name="name" />
+					<input
+						type="text"
+						name="name"
+						value={this.state.value}
+						onChange={this.onChange}
+					/>
 				</label>
+				<label htmlFor="">Leave a Comment:</label>
+				<textarea
+					name=""
+					id=""
+					value={this.state.value}
+					onChange={this.onChange}
+				></textarea>
+				<label htmlFor="">Choose Animal:</label>
+				<select name="animals" id="animals" onChange={this.onChange}>
+					<option value="dog">dog</option>
+					<option value="cat">cat</option>
+					<option value="snake">snake</option>
+					<option value="bird">bird</option>
+				</select>
 				<input type="submit" value="Submit" />
 			</form>
 		);
---
+++ react-forms/src/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+const age = 20;
+let username = "Michael Scott";
+console.log(username);
